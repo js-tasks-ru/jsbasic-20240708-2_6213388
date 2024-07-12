82d996ab6a781ed30b301d4b8bf52907cd6de2fb
Viktor191 <109589393+Viktor191@users.noreply.github.com>
2024-07-12 15:00:59 +0200

    added username validation function

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,20 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if(name === null) {
+
+    return false;
+  }
+  if (name.indexOf(' ') !== -1) {
+
+    return false;
+  }
+  if (name.length < 4 || name.length === 0) {
+
+    return false;
+  }
+
+  return true;
 }
 
 function sayHello() {
