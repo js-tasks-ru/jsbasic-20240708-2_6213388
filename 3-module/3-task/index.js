c58eecb83493cf3ae1772cafcc8ab3b21b8e7db8
Viktor191 <109589393+Viktor191@users.noreply.github.com>
2024-07-24 15:43:50 +0200

    exercises 1 to 5 solved

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  let strParts = str.split('-');
+  let result;
+
+  result = strParts.map((element, index) => {
+    if (index === 0) {
+      return element;
+    } else {
+      return element.charAt(0).toUpperCase() + element.slice(1);
+    }
+  });
+
+  return  result.join('');
 }
